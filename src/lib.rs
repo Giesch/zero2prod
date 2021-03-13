8c7932559d7805268dba2a80d0289d1270f60190
Dan Knutson <dan.knutson@gmail.com>
2021-03-13 10:07:39 -0800

    remove clippy allow

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-// TODO remove this once sqlx releases the fix
-// https://github.com/rust-lang/rust-clippy/issues/5849
-#![allow(clippy::toplevel_ref_arg)]
 pub mod configuration;
 pub mod domain;
 pub mod email_client;
